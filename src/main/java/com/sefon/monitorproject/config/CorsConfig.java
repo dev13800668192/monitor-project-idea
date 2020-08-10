46b75bc1b9d7c83e2caaf3ae6c8a57f5e8c190df
by <wb13880668192@163.com>
2020-08-10 14:27:43 +0800

    2020.8.10 解决跨域

---
+++ src/main/java/com/sefon/monitorproject/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.sefon.monitorproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author by
+ * @version 1.0
+ * @date 2020/8/10 13:27
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
